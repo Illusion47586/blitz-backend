24328700f5dd43c8d1ce72543d24ea07999ab93c
illusion <dhruvtiwari110058@gmail.com>
2021-11-02 13:56:48 +0530

    dotenv config

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+require("dotenv").config();
 
 require("./db/mongoose");
 const frequencyRouter = require("./routers/frequency");
