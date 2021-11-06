e30f8972792d161b692906aa58b4388e21024a4e
illusion <dhruvtiwari110058@gmail.com>
2021-11-06 23:21:25 +0530

    new database

--- src/server.js
+++ src/server.js
@@ -11,6 +11,11 @@ const { router: cloudinaryRouter } = require("./routers/cloudinary");
 const app = express();
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
   next();
 });
 
